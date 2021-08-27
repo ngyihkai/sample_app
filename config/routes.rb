417419e998d8815904b02db43d205671676f41dd
ngyihkai <ngyihkai12@gmail.com>
2021-08-27 11:54:02 +0800

    chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|ja/ do
-    root 'static_pages#home'
-    get 'static_pages/help'
+    root 'static_pages#home', as: "home"
+    get 'static_pages/help', as: "help"
+
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new create show)
   end
 end
