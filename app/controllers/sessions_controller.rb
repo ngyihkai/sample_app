ec56d91f037a5877e455423aa2942cf8f9f2f3d0
ngyihkai <ngyihkai12@gmail.com>
2021-08-30 17:16:15 +0800

    chapter_8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      logged_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = t "invalid_email_password_combination"
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,24 @@
 class User < ApplicationRecord
-  has_secure_password
-
+  attr_accessor :remember_token
   before_save :downcase_email
 
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  has_secure_password
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 
   private
 
@@ -13,12 +26,18 @@ class User < ApplicationRecord
     self.email.downcase!
   end
 
-  def User.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-            BCrypt::Engine::MIN_COST
-           else
-            BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost: cost
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+             else
+              BCrypt::Engine.cost
+             end
+              BCrypt::Password.create string, cost: cost
+    end
   end
 end
