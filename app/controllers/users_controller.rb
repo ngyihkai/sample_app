89a2bbd3ab4c3ce20e1e9706adbedb956b7695a4
ngyihkai <ngyihkai12@gmail.com>
2021-09-01 15:46:46 +0800

    chapter_10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
   before_action :load_user, only: :show
+
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show; end
 
@@ -17,6 +25,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user&.destroy
+      flash[:success] = t(:User_deleted)
+    else
+        flash[:danger] = t(:Delete_fail)
+    end
+    redirect_to users_url
+  end
+
   private
 
   def user_params
@@ -29,5 +57,24 @@ class UsersController < ApplicationController
 
     flash[:error] = t(:error)
     redirect_to root_path
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t(:Please_log_in)
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    return if current_user?(@user)
+    flash[:danger]= t(:you_are_not_authorized)
+    redirect_to(root_url)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Example User",
+             email:"example@railstutorial.org",
+             password: "111111",
+             password_confirmation: "111111",
+             admin: true)
+
+User.create!(name: "Example User",
+             email:"welcome@railstutorial.org",
+             password: "111111",
+             password_confirmation: "111111",
+             admin: true)
+
+50.times do |n|
+  name = "Example #{n+1}"
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
